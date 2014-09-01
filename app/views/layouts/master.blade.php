c2bcefa37386718020694bcbabb0598d35542346
Mike McMullan <apps@mike-mcmullan.com>
2014-09-01 19:11:06 -0400

    Now using NHL favicon instead of just the maple leafs favicon.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('pageTitle')</title>
 
-    <link rel="shortcut icon" type="image/x-icon" href="http://cdn.nhle.com/mapleleafs/v2/images/favicon.ico"></link>
+    <link rel="shortcut icon" type="image/x-icon" href="http://cdn.nhle.com/nhl/images/favicon.ico"></link>
     {{ HTML::style('assets/css/style.css') }}
     {{ HTML::script('http://code.jquery.com/jquery-2.1.1.min.js') }}
 </head>
