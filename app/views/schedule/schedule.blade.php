cb250f633ab78a8596744b821b404d845fa51fcd
Mike McMullan <apps@mike-mcmullan.com>
2014-09-27 02:08:45 -0400

    Removed unnecessary span.

--- app/views/schedule/schedule.blade.php
+++ app/views/schedule/schedule.blade.php
@@ -32,17 +32,13 @@ NHL {{ $teamName }} Schedule &#8250; 2014 - 2015 NHL Schedule Viewer
         <div class="date"><span>{{ $match['date']->format('D\, M jS\, Y') }}</span></div>
 
         <div class="visitor">
-            <span>
-                <span class="full-team-name">{{ link_to_route('team_schedule_path', getTeamShortName($match['away_team']), [$match['away_team']]) }}</span>
-                <span class="team-id">{{ link_to_route('team_schedule_path', $match['away_team'], [$match['away_team']]) }}</span>
-            </span>
+            <span class="full-team-name">{{ link_to_route('team_schedule_path', getTeamShortName($match['away_team']), [$match['away_team']]) }}</span>
+            <span class="team-id">{{ link_to_route('team_schedule_path', $match['away_team'], [$match['away_team']]) }}</span>
         </div>
 
         <div class="home">
-            <span>
-                <span class="full-team-name">{{ link_to_route('team_schedule_path', getTeamShortName($match['home_team']), [$match['home_team']]) }}</span>
-                <span class="team-id">{{ link_to_route('team_schedule_path', $match['home_team'], [$match['home_team']]) }}</span>
-            </span>
+            <span class="full-team-name">{{ link_to_route('team_schedule_path', getTeamShortName($match['home_team']), [$match['home_team']]) }}</span>
+            <span class="team-id">{{ link_to_route('team_schedule_path', $match['home_team'], [$match['home_team']]) }}</span>
         </div>
 
         <div class="time"><span>{{ $match['date']->format('g:i A') }}</span></div>
