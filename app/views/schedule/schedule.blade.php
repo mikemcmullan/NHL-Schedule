790f21bdbf147395470f886a1d38b0ef5197f6b0
Mike McMullan <apps@mike-mcmullan.com>
2014-10-16 17:05:48 -0400

    Moved schedule row to partial for use in more than one place.

--- app/views/schedule/schedule.blade.php
+++ app/views/schedule/schedule.blade.php
@@ -27,29 +27,7 @@ NHL {{ $teamName }} Schedule &#8250; 2014 - 2015 NHL Schedule Viewer
 
         @foreach ($matchGroup as $match)
 
-    <div class="schedule-row {{ isset($match['nextGame']) ? 'next-game ' : '' }}{{ isCollapsed($match) ? 'collapsed' : '' }}" data-month="{{ $matchGroup->first()['date']->format('Y-m') }}">
-
-        <div class="date"><span>{{ $match['date']->format('D\, M jS\, Y') }}</span></div>
-
-        <div class="visitor">
-            <span class="full-team-name">{{ link_to_route('team_schedule_path', getTeamShortName($match['away_team']), [$match['away_team']]) }}</span>
-            <span class="team-id">{{ link_to_route('team_schedule_path', $match['away_team'], [$match['away_team']]) }}</span>
-        </div>
-
-        <div class="home">
-            <span class="full-team-name">{{ link_to_route('team_schedule_path', getTeamShortName($match['home_team']), [$match['home_team']]) }}</span>
-            <span class="team-id">{{ link_to_route('team_schedule_path', $match['home_team'], [$match['home_team']]) }}</span>
-        </div>
-
-        <div class="time"><span>{{ $match['date']->format('g:i A') }}</span></div>
-
-        @if ( ! $match['results'])
-        <div class="results"><span>{{ $match['tv_info'] }}</span></div>
-        @else
-        <div class="results"><span>{{ $match['results'] }}</span></div>
-        @endif
-
-    </div>
+            @include('schedule.partials.schedule-row', ['match' => $match, 'yearMonth' => $matchGroup->first()['date']->format('Y-m')])
 
         @endforeach
 
