2512ef94add14b693b995022bfa95fd05c9608a9
Mike McMullan <apps@mike-mcmullan.com>
2014-10-23 23:58:52 -0400

    Added route to display matches for a given day. Feature not yet finished.

--- app/routes.php
+++ app/routes.php
@@ -16,3 +16,24 @@ Route::get('/team/{id}/schedule', [
     'as'    => 'team_schedule_path',
     'uses'  => 'TeamController@schedule'
 ]);
+
+Route::pattern('date', '[\d]{4}-[\d]{2}-[\d]{2}');
+
+Route::bind('date', function($value, $route)
+{
+    try
+    {
+        return \Carbon\Carbon::parse($value);
+    }
+    catch(Exception $e)
+    {
+        App::abort(404);
+    }
+});
+
+Route::get('{date}', [function(\Carbon\Carbon $date)
+{
+    $divisions = Config::get('nhl.conferences');
+
+    return View::make('home')->withDivisions($divisions);
+}]);
