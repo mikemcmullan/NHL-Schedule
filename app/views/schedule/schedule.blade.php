7ad4c167db0828fe72554e223cca1e7153eed46c
Mike McMullan <apps@mike-mcmullan.com>
2015-09-05 14:11:41 -0400

    Changed page titles.

--- app/views/schedule/schedule.blade.php
+++ app/views/schedule/schedule.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('pageTitle')
-NHL {{ $teamName }} Schedule &#8250; 2014 - 2015 NHL Schedule Viewer
+NHL {{ $teamName }} Schedule &#8250; 2015 - 2016 NHL Schedule Viewer
 @stop
 
 @section('content')
